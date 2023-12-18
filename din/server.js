d34faf725874c24b2e6c2508a07e739103796f41
Алексей Боклогов <kashira87@mail.ru>
2023-12-18 16:52:17 +0300

    does add scripts in package.json

--- din/server.js
+++ din/server.js
@@ -4,5 +4,5 @@ const app = require('../app')
 const { PORT = 3000 } = process.env
 
 app.listen(PORT, () => {
-  console.log(`Сервер запущен на порту ${PORT}`)
+  console.log(`Server started on port: ${PORT}`)
 })
