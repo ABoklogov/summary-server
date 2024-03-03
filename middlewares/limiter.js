d376c486816b8dfc1d19bc11cf71a3386f05afba
root <root@yufeklufqs.local>
2024-03-03 09:01:29 +0000

    Увеличил максимальное число запросов до 10

--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // ожидание - 15 минут
-  max: 5, // максимальное число запросов - 5
+  max: 10, // максимальное число запросов - 10
   handler: (req, res) => {
     res.status(403).json({
       code: 403,
