52dc0a959a402a2c7c159a06ca636fdd69798a29
Алексей Боклогов <kashira87@mail.ru>
2024-03-25 16:57:10 +0300

    Исправил модель Links

--- models/portfolio/link.js
+++ models/portfolio/link.js
@@ -4,17 +4,15 @@ const Joi = require('joi');
 const linkSchema = Schema({
   linkClient: {
     type: String,
-    required: true
   },
   linkServer: {
     type: String,
-    required: true
   },
 }, { versionKey: false, timestamps: true });
 
 const joiSchemaLinks = Joi.object({
-  linkClient: Joi.string().required(),
-  linkServer: Joi.string().required(),
+  linkClient: Joi.string(),
+  linkServer: Joi.string(),
 });
 
 const Link = model('link', linkSchema);
